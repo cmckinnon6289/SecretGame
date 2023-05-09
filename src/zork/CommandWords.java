57313fdb3db27eef03882e70de8479edab2d12a9
Abraham Zhang <zzhang@bayviewglen.ca>
2023-05-09 11:15:05 -0400

    completed command words

--- src/zork/CommandWords.java
+++ src/zork/CommandWords.java
@@ -1,38 +1,53 @@
 package zork;
 
+
 public class CommandWords {
-  // a constant array that holds all valid command words
-  private static final String validCommands[] = { "go", "quit", "help", "eat", "get", "run", 
-  "get", "grab", "jump", "forward", "backward", "left", "right", "up", "down", 
-  "drink", "eat", "open", "close", "look", "search", "strike", "fight", "rest", "heal", "north", "east", "west", "south"};
+    // a constant array that holds all valid command words
+    private static final String[] VALID_COMMANDS = {
+            "go", "quit", "help", "eat", "get", "run", "grab", "jump", "forward",
+            "backward", "left", "right", "up", "down", "drink", "open", "close",
+            "look", "search", "strike", "fight", "rest", "heal", "north", "east",
+            "west", "south", "exit", "attack", "find", "view", "pick up"
+    };
 
-  /**
-   * Constructor - initialise the command words.
-   */
-  public CommandWords() {
-    // nothing to do at the moment...
-  }
-
-  /**
-   * Check whether a given String is a valid command word. Return true if it is,
-   * false if it isn't.
-   **/
-  public boolean isCommand(String aString) {
-    for (String c : validCommands) {
-      if (c.equals(aString))
-        return true;
+    /**
+     * Check whether a given String is a valid command word. Return true if it is,
+     * false if it isn't.
+     **/
+    public static boolean isCommand(String aString) {
+        for (String c : VALID_COMMANDS) {
+            if (c.equalsIgnoreCase(aString))
+                return true;
+        }
+        // if we get here, the string was not found in the commands
+        return false;
     }
-    // if we get here, the string was not found in the commands
-    return false;
-  }
 
-  /*
-   * Print all valid commands to System.out.
-   */
-  public void showAll() {
-    for (String c : validCommands) {
-      System.out.print(c + "  ");
+    /**
+     * Returns the canonical version of a command word, resolving synonyms if
+     * necessary. Returns null if the input is not a valid command.
+     */
+    public static String getLowerCaseForm(String input) {
+        // First check if the input is already a valid command
+
+        String lowerCase = input.toLowerCase();
+
+        if (isCommand(lowerCase)) {
+            return lowerCase;
+        }
+
+        // If we get here, the input is not a valid command or synonym
+        return null;
     }
-    System.out.println();
-  }
+
+    /*
+     * Print all valid commands to System.out.
+     */
+    public static void showAll() {
+        for (String c : VALID_COMMANDS) {
+            System.out.print(c + "  ");
+        }
+        System.out.println();
+    }
 }
+
