50a5d047ee4457ea3fb203f8d5c0c67c9a865d9b
Sebastian Cheng <scheng1@bayviewglen.ca>
2023-05-15 11:29:14 -0400

    wait NOW i fixed Lantern

--- src/zork/Lantern.java
+++ src/zork/Lantern.java
@@ -2,13 +2,15 @@ package zork;
 
 public class Lantern extends Item {
    boolean isOn;
+   Room lanternRoom;
     public Lantern(double weight, String name, boolean isOpenable, boolean isOn, Room currentRoom){
         super(weight, name, isOpenable, currentRoom);
         this.isOn = isOn;
+        this.lanternRoom = currentRoom;
     }
     public void use() {
         System.out.print("The lantern gives off weak light. ");
-        if (Item.getRoomItems(super.currentRoom) != null) {
+        if (Item.getRoomItems(lanternRoom) != null) {
             System.out.println("Something glimmers in the corner in the room.");
         } else {
             System.out.println("Nothing catches your eye.");
