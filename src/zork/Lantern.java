2638cca6aa4c29a87324e65a99c9f85c05e6f510
cmckinnon6289 <105380433+cmckinnon6289@users.noreply.github.com>
2023-05-23 11:07:43 -0400

    printing items
    yep

--- src/zork/Lantern.java
+++ src/zork/Lantern.java
@@ -22,6 +22,13 @@ public class Lantern extends Item {
     public void useLantern() {
         if(isOn == true)
           System.out.print("The lantern gives off weak light. ");
+          ArrayList<Item> items = Item.getRoomItems(Game.getCurrentRoom());
+          if (items != null) {
+             System.out.print("You see: "
+             items.forEach((item) -> if (!item.isMonster) {System.out.println(item+", ")});
+             System.out.println("and nothing else.")
+          } else {
+             System.out.println("You see nothing of value in the room.");
         else if(isOn == false)
           System.out.println("The lantern is not on");
     }
