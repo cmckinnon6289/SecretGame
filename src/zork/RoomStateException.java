00cb52475944cf3d1092594161b758842623a650
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2023-05-25 11:21:52 -0400

    rooms done (!!)

--- src/zork/RoomStateException.java
+++ src/zork/RoomStateException.java
@@ -1,12 +1,12 @@
 package zork;
 
-public class DuplicateRoomException extends Exception {
-    public DuplicateRoomException() {
-        super("room already exists with these coordinates");
+public class RoomStateException extends Exception {
+    public RoomStateException() {
+        super("problem with a room");
         printStackTrace();
         System.exit(-1);
     }
-    public DuplicateRoomException(CoordKey key) {
+    public RoomStateException(CoordKey key) {
         super("room already exists with coordinates: "+key.exceptionPrint());
         printStackTrace();
         System.exit(-1);
