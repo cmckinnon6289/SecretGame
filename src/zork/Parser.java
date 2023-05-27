ec9687ad94399e4563e683402acbc3cef3ebf76d
cmckinnon6289 <105380433+cmckinnon6289@users.noreply.github.com>
2023-05-27 17:53:23 -0400

    parser can take 3 words
    i think at least

--- src/zork/Parser.java
+++ src/zork/Parser.java
@@ -23,8 +23,15 @@ public class Parser {
 
     String word1 = words[0];
     String word2 = null;
-    if (words.length > 1)
+    String word3 = null;
+    if (words.length > 1) {
       word2 = words[1];
+      try {
+        word3 = words[2];
+      } catch(ArrayIndexOutOfBoundsException e) {
+        word3 = null;
+      }
+    }
 
     if (commands.isCommand(word1))
       return new Command(word1, word2);
